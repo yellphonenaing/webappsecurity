a381c3dcebb29576afa41f595aa5f05b6ca63e07
ypn007 <55645320+ypn007@users.noreply.github.com>
2024-10-29 15:02:45 +0630

    Create even_odd.php

---
+++ PHP/even_odd.php
@@ -0,0 +1,8 @@
+<?php
+$number = (int) readline("Enter a number :: ");
+if ($number % 2 === 0) {
+    echo "Your number $number is an even number.\n";
+} else {
+    echo "Your number $number is an odd number.\n";
+}
+?>
